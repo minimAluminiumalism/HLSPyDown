07c46882acaa7f8c464a45e38a8aca5058e97ce4
Cuesen Tsai <caixuesen@outlook.com>
2019-08-12 17:44:44 +0800

    new function added: progress bar display while downloading.

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-import requests
+import time
+from progressbar import *
 
-key_uri = "https://p2.japronx.com/keys/bbe45cc0125227aab4ff0639d915fca5.key"
+total = 1000
 
-index = 1
 
-while index <= 100:
-    response = requests.get(key_uri)
-    print(response.status_code)
-    index += 1
+def dosomework():
+    time.sleep(0.01)
+
+
+widgets = ['Progress: ', Percentage(), ' ', Bar('#'), ' ', Timer(),
+           ' ', ETA(), ' ', FileTransferSpeed()]
+pbar = ProgressBar(widgets=widgets, maxval=10 * total).start()
+for i in range(total):
+    # do something
+    pbar.update(10 * i + 1)
+    dosomework()
+
+pbar.finish()
